5dbc2eb6875677d58d29e8d2befc6f938b0990ee
dzeble <dzeble>
2024-02-07 14:58:30 +0000

    Trying again

---
+++ experiment_tracking/wine-tracking.py
@@ -0,0 +1,429 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#imports for data exploration and analysis
+import pandas as pd
+#importing models
+from sklearn.ensemble import ExtraTreesClassifier, RandomForestClassifier
+
+from sklearn.metrics import mean_squared_error, r2_score
+
+from sklearn.preprocessing import OneHotEncoder
+
+import pickle
+
+
+# In[3]:
+
+
+import mlflow
+
+mlflow.set_tracking_uri("sqlite:///mlflow.db")
+mlflow.set_experiment("wine-quality-experiment")
+
+
+# In[4]:
+
+
+from sklearn.preprocessing import LabelEncoder
+
+def read_dafaframe(filename):
+    df = pd.read_csv(filename)
+
+    le = LabelEncoder()
+    df['type'] = le.fit_transform(df['type'])
+
+    encoder = OneHotEncoder()
+    encoded_types = encoder.fit_transform(df['type'].values.reshape(-1, 1))
+
+    label = le.classes_.tolist()
+    wine_types = pd.DataFrame(encoded_types.toarray(), columns=label)
+
+    df = pd.concat([df, wine_types], axis=1)
+
+    for col, value in df.items():
+        if col != 'type':
+            df[col] = df[col].fillna(df[col].mean())
+
+    return df
+
+
+# In[5]:
+
+
+train_df = read_dafaframe('./wine_data/train_wine_data.csv')
+validation_df = read_dafaframe('./wine_data/test_wine_data.csv')
+
+
+trained_dict =  train_df.drop(columns=['type','quality'])
+val_dict =  validation_df.drop(columns=['type','quality'])
+
+
+X_train = trained_dict
+X_val = val_dict
+
+
+target = 'quality'
+y_train = train_df[target].values
+
+y_val = validation_df[target].values
+
+
+# In[6]:
+
+
+rfc = RandomForestClassifier(n_estimators=100)
+
+rfc.fit(X_train, y_train)
+y_pred_rfc = rfc.predict(X_val)
+
+rmse = mean_squared_error(y_val,y_pred_rfc,squared=False)
+accuracy = r2_score(y_val, y_pred_rfc)
+
+print('RandomForestClassifier')
+print(f'RMSE: {rmse}')
+print(f'Accuracy: {accuracy}')
+
+
+# In[7]:
+
+
+etc = ExtraTreesClassifier(n_estimators=100)
+
+etc.fit(X_train, y_train)
+y_pred_etc = etc.predict(X_val)
+
+rmse = mean_squared_error(y_val,y_pred_etc,squared=False)
+accuracy = r2_score(y_val,y_pred_etc)
+
+print('ExtraTreesClassifier')
+print (f'RMSE: ',{rmse})
+print(f'Accuracy: ',{accuracy})
+
+
+# In[8]:
+
+
+with open('models/etc.bin','wb') as f_out:
+    pickle.dump((etc),f_out)
+
+
+# In[40]:
+
+
+with mlflow.start_run():
+
+    mlflow.set_tag("developer","Sven")
+
+    mlflow.log_param("train-data-path", "../wine_data/train_wine_data.csv")
+    mlflow.log_param("valid-data-path", "../wine_data/test_wine_data.csv")
+
+    n_estimators = 100
+    mlflow.log_param('n_estimators',n_estimators)
+
+    etc = ExtraTreesClassifier(n_estimators=n_estimators)
+
+    etc.fit(X_train, y_train)
+    y_pred_etc = etc.predict(X_val)
+
+    rmse = mean_squared_error(y_val,y_pred_etc,squared=False)
+    accuracy = r2_score(y_val,y_pred_etc)
+    
+    print('ExtraTreesClassifier')
+    print (f'RMSE: ',{rmse})
+    print(f'Accuracy: ',{accuracy})
+
+    mlflow.log_metric("rmse", rmse)
+    mlflow.log_metric("accuracy", accuracy)
+    mlflow.set_tag("model", "ExtraTreesClassifier")
+
+    mlflow.log_artifact("models/etc.bin",artifact_path="sklearn_model")
+    
+    mlflow.sklearn.log_model(etc,"model")
+
+
+# In[10]:
+
+
+import xgboost as xgb
+from hyperopt import  fmin, tpe, hp, STATUS_OK, Trials
+from hyperopt.pyll import scope
+
+
+# In[11]:
+
+
+train = xgb.DMatrix(X_train, label=y_train)
+valid = xgb.DMatrix(X_val, label=y_val)
+
+
+# In[12]:
+
+
+def objective(params):
+    with mlflow.start_run():
+        mlflow.set_tag("model", "xgboost")
+        mlflow.log_params(params)
+        booster = xgb.train(
+            params=params,
+            dtrain=train,
+            num_boost_round=1000,
+            evals=[(valid, 'validation')],
+            early_stopping_rounds=50
+        )
+        y_pred = booster.predict(valid)
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
+        accuracy = r2_score(y_val,y_pred)
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("accuracy", accuracy)
+
+    return {'loss': rmse, 'accuracy':accuracy, 'status': STATUS_OK}
+
+
+# In[13]:
+
+
+search_space = {
+    'max_depth': scope.int(hp.quniform('max_depth', 4, 100, 1)),
+    'learning_rate': hp.loguniform('learning_rate', -3, 0),
+    'reg_alpha': hp.loguniform('reg_alpha', -5, -1),
+    'reg_lambda': hp.loguniform('reg_lambda', -6, -1),
+    'min_child_weight': hp.loguniform('min_child_weight', -1, 3),
+    'objective': 'reg:linear',
+    'seed': 42
+}
+
+best_result = fmin(
+    fn=objective,
+    space=search_space,
+    algo=tpe.suggest,
+    max_evals=50,
+    trials=Trials()
+)
+
+
+# In[25]:
+
+
+params={
+    'learning_rate':	0.19810073258268465,
+    'max_depth':	8,
+    'min_child_weight':	3.6166554203005914,
+    'objective':	'reg:linear',
+    'reg_alpha':	0.1778602103708601,
+    'reg_lambda':	0.026355351557057448,
+    'seed':	42
+}
+
+mlflow.xgboost.autolog(disable=True)
+
+booster = xgb.train(
+    params=params,
+    dtrain=train,
+    num_boost_round=1000,
+    evals=[(valid, 'validation')],
+    early_stopping_rounds=50
+)
+
+
+
+# In[16]:
+
+
+with mlflow.start_run():
+
+    mlflow.set_tag("developer","Sven")
+
+    mlflow.log_param("train-data-path", "../wine_data/train_wine_data.csv")
+    mlflow.log_param("valid-data-path", "../wine_data/test_wine_data.csv")
+
+    n_estimators = 100
+    mlflow.log_param('n_estimators',n_estimators)
+
+    etc = ExtraTreesClassifier(n_estimators=n_estimators)
+
+    etc.fit(X_train, y_train)
+    y_pred_etc = etc.predict(X_val)
+
+    rmse = mean_squared_error(y_val,y_pred_etc,squared=False)
+    accuracy = r2_score(y_val,y_pred_etc)
+    
+    print('ExtraTreesClassifier')
+    print (f'RMSE: ',{rmse})
+    print(f'Accuracy: ',{accuracy})
+
+    mlflow.log_metric("rmse", rmse)
+    mlflow.log_metric("accuracy", accuracy)
+    
+    mlflow.log_artifact(local_path="models/etc.bin", artifact_path="models_pickle")
+
+
+# In[20]:
+
+
+booster.save_model('models/xgb.bin')
+
+
+# In[21]:
+
+
+params={
+    'learning_rate':	0.19810073258268465,
+    'max_depth':	8,
+    'min_child_weight':	3.6166554203005914,
+    'objective':	'reg:linear',
+    'reg_alpha':	0.1778602103708601,
+    'reg_lambda':	0.026355351557057448,
+    'seed':	42
+}
+
+
+booster = xgb.train(
+    params=params,
+    dtrain=train,
+    num_boost_round=1000,
+    evals=[(valid, 'validation')],
+    early_stopping_rounds=50
+)
+
+y_pred = booster.predict(valid)
+rmse = mean_squared_error(y_val, y_pred, squared=False)
+accuracy = r2_score(y_val,y_pred)
+mlflow.log_metric("rmse", rmse)
+mlflow.log_metric("accuracy", accuracy)
+mlflow.set_tag("model", "xgboost")
+mlflow.log_params(params)
+
+mlflow.log_artifact(local_path="models/xgb.bin", artifact_path="models_pickle")
+
+
+# In[26]:
+
+
+if mlflow.active_run():
+    mlflow.end_run()
+
+with mlflow.start_run():
+    
+    best_params={
+    'learning_rate':	0.19810073258268465,
+    'max_depth':	8,
+    'min_child_weight':	3.6166554203005914,
+    'objective':	'reg:linear',
+    'reg_alpha':	0.1778602103708601,
+    'reg_lambda':	0.026355351557057448,
+    'seed':	42
+}
+
+    mlflow.log_params(best_params)
+
+    booster = xgb.train(
+        params=best_params,
+        dtrain=train,
+        num_boost_round=1000,
+        evals=[(valid, 'validation')],
+        early_stopping_rounds=50
+    )
+
+    y_pred = booster.predict(valid)
+    rmse = mean_squared_error(y_val, y_pred, squared=False)
+    accuracy = r2_score(y_val,y_pred)
+    mlflow.log_metric("rmse", rmse)
+    mlflow.log_metric("accuracy", accuracy)
+    mlflow.set_tag("model", "xgboost")
+
+
+    with open("models/xgb.bin","wb") as f_out:
+        pickle.dump(booster,f_out)
+    
+    mlflow.log_artifact("models/xgb.bin",artifact_path="xgboost_model")
+
+    mlflow.xgboost.log_model(booster,artifact_path="models_mlflow")
+
+
+# In[27]:
+
+
+logged_model = 'runs:/972efc8246b347e8beb77b09ad439e4d/models_mlflow'
+
+# Load model as a PyFuncModel.
+loaded_model = mlflow.pyfunc.load_model(logged_model)
+
+
+# In[28]:
+
+
+loaded_model
+
+
+# In[29]:
+
+
+xgboost_model = mlflow.xgboost.load_model(logged_model)
+
+
+# In[30]:
+
+
+xgboost_model
+
+
+# In[31]:
+
+
+new_y_pred = xgboost_model.predict(valid)
+
+
+# In[32]:
+
+
+new_y_pred[:10]
+
+
+# In[41]:
+
+
+with mlflow.start_run():
+
+    mlflow.set_tag("developer","Sven")
+
+    mlflow.log_param("train-data-path", "../wine_data/train_wine_data.csv")
+    mlflow.log_param("valid-data-path", "../wine_data/test_wine_data.csv")
+
+    n_estimators = 100
+    mlflow.log_param('n_estimators',n_estimators)
+
+    rfc = RandomForestClassifier(n_estimators=100)
+
+    rfc.fit(X_train, y_train)
+    y_pred_rfc = rfc.predict(X_val)
+
+    rmse = mean_squared_error(y_val,y_pred_rfc,squared=False)
+    accuracy = r2_score(y_val,y_pred_rfc)
+
+    mlflow.log_metric("rmse", rmse)
+    mlflow.log_metric("accuracy", accuracy)
+    mlflow.set_tag("model", "RandomForestClassifier")
+
+    mlflow.log_artifact("models/rfc.bin",artifact_path="sklearn_model")
+
+  
+    
+    mlflow.sklearn.log_model(rfc,"model")
+
+
+# In[35]:
+
+
+with open('models/rfc.bin','wb') as f_out:
+    pickle.dump((rfc),f_out)
+
+
+# In[ ]:
+
+
+
+
---
+++ wine_quality_training/wine_training.py
@@ -0,0 +1,450 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#imports for data exploration and analysis
+import pandas as pd
+import numpy as np
+#imports for data visualization
+import matplotlib.pyplot as plt
+import seaborn as sns
+#importing models
+from sklearn.ensemble import RandomForestRegressor, ExtraTreesClassifier, RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import mean_squared_error, r2_score
+
+import pickle
+
+import warnings 
+warnings.filterwarnings('ignore')
+
+
+# In[2]:
+
+
+#training data
+wine_train_df = pd.read_csv('wine_data/train_wine_data.csv') 
+wine_train_df.head()
+
+
+# In[3]:
+
+
+wine_train_df.info()
+
+
+# In[4]:
+
+
+wine_train_df.isnull().sum()
+
+
+# In[5]:
+
+
+wine_train_df.describe()
+
+
+# In[6]:
+
+
+# create box plots
+fig, ax = plt.subplots(ncols=6, nrows=2, figsize=(20,10))
+index = 0
+ax = ax.flatten()
+for col, value in wine_train_df.items():
+ if col != 'type':
+    sns.boxplot(y=col, data=wine_train_df, ax=ax[index])
+    index += 1
+plt.tight_layout(pad=0.5, w_pad=0.7, h_pad=5.0)
+
+
+# In[7]:
+
+
+#checking which values affect wine quality
+wine_test = wine_train_df.corr()
+plt.figure(figsize = (20,20))
+sns.heatmap(wine_test, cmap = 'Blues', annot = True)
+
+
+# In[8]:
+
+
+#checking if the type of wine will affect the quality
+sns.barplot(data=wine_train_df, x='type', y='quality')
+plt.show()
+
+
+# In[9]:
+
+
+#distribution of wine quality
+sns.distplot(wine_train_df['quality'])
+
+
+# In[10]:
+
+
+wine_train_df['quality'].describe(percentiles=[0.95, 0.98, 0.99])
+
+
+# In[11]:
+
+
+from sklearn.preprocessing import OneHotEncoder
+
+#encoding the wine types as 1 and 0 with respect to red to be computated
+
+encoder = OneHotEncoder(sparse=False)
+
+encode_types = encoder.fit_transform(wine_train_df['type'].values.reshape(-1, 1))
+encode_types
+
+
+# In[12]:
+
+
+encoder.categories_
+
+
+# In[13]:
+
+
+label= ['red','white']
+wine_types = pd.DataFrame(encode_types, columns= label)
+
+
+# In[14]:
+
+
+wine_types.value_counts()
+
+
+# In[15]:
+
+
+wine_types['red'].value_counts()
+
+
+# In[16]:
+
+
+#confirming the presence of the encoded column
+wine_train_df['red_wine'] = wine_types['red']
+
+wine_train_df.head()
+
+
+# In[17]:
+
+
+X_train = wine_train_df.drop(columns=['type','quality'])
+X_train
+
+
+# In[18]:
+
+
+target = 'quality'
+y_train = wine_train_df[target].values
+y_train
+
+
+# In[19]:
+
+
+#trying with random forest regression
+
+rfr = RandomForestRegressor(n_estimators=100, random_state=42)
+rfr.fit(X_train, y_train)
+
+y_pred = rfr.predict(X_train)
+
+rmse = mean_squared_error(y_train,y_pred,squared=False)
+accuracy = r2_score(y_train,y_pred)
+
+print('RandomForestRegressor')
+print (f'RMSE: ',{rmse})
+print(f'Accuracy: ',{accuracy})
+
+
+# In[20]:
+
+
+sns.distplot(y_pred, label = 'prediction')
+sns.distplot(y_train, label = 'actual')
+plt.title(f'RandomForestRegressor')
+
+plt.legend()
+
+
+# In[21]:
+
+
+#trying with random forest classifier
+
+rfc = RandomForestClassifier(n_estimators=100)
+rfc.fit(X_train, y_train)
+
+y_pred = rfc.predict(X_train)
+
+rmse = mean_squared_error(y_train,y_pred,squared=False)
+accuracy = r2_score(y_train,y_pred)
+
+print('RandomForestClassifier')
+print (f'RMSE: ',{rmse})
+print(f'Accuracy: ',{accuracy})
+
+
+# In[22]:
+
+
+sns.distplot(y_pred, label = 'prediction')
+sns.distplot(y_train, label = 'actual')
+plt.title('RandomForestClassifier')
+
+plt.legend()
+
+
+# In[23]:
+
+
+#trying with extra trees classifier
+
+etc = ExtraTreesClassifier(n_estimators=100)
+etc.fit(X_train, y_train)
+
+y_pred = etc.predict(X_train)
+
+rmse = mean_squared_error(y_train,y_pred,squared=False)
+accuracy = r2_score(y_train,y_pred)
+
+print('ExtraTreesClassifier')
+print (f'RMSE: ',{rmse})
+print(f'Accuracy: ',{accuracy})
+
+
+# In[24]:
+
+
+sns.distplot(y_pred, label = 'prediction')
+sns.distplot(y_train, label = 'actual')
+plt.title('ExtraTreesClassifier')
+
+plt.legend()
+
+
+# In[25]:
+
+
+#trying with extra trees classifier
+
+dtc =  DecisionTreeClassifier()
+dtc.fit(X_train, y_train)
+
+y_pred = etc.predict(X_train)
+
+rmse = mean_squared_error(y_train,y_pred,squared=False)
+accuracy = r2_score(y_train,y_pred)
+
+print('DecisionTreeClassifier')
+print (f'RMSE: ',{rmse})
+print(f'Accuracy: ',{accuracy})
+
+
+# In[26]:
+
+
+sns.distplot(y_pred, label = 'prediction')
+sns.distplot(y_train, label = 'actual')
+plt.title('DecisionTreeClassifier')
+
+plt.legend()
+
+
+# In[27]:
+
+
+#trying with kNeighbors Regression
+
+knr = KNeighborsRegressor(n_neighbors=5) 
+knr.fit(X_train, y_train)
+
+y_pred = knr.predict(X_train)
+
+rmse = mean_squared_error(y_train,y_pred,squared=False)
+accuracy = r2_score(y_train,y_pred)
+
+
+print (f'RMSE: ',{rmse})
+print(f'Accuracy: ',{accuracy})
+
+
+# In[28]:
+
+
+sns.distplot(y_pred, label = 'prediction')
+sns.distplot(y_train, label = 'actual')
+
+plt.title('KNeighborsRegressor')
+plt.legend()
+
+
+# In[29]:
+
+
+#reading the data with a function to avoid too much repetition
+def read_dafaframe(filename):
+    df = pd.read_csv(filename)
+
+    encoder = OneHotEncoder(sparse=False)
+
+    encoded_types = encoder.fit_transform(df['type'].values.reshape(-1, 1))
+
+    label= ['red','white']
+    wine_types = pd.DataFrame(encoded_types, columns= label)
+
+    df['red_wine'] = wine_types['red']
+
+    return df
+
+
+# In[30]:
+
+
+train_df = read_dafaframe('wine_data/train_wine_data.csv')
+#adding a validation dataset
+validation_df = read_dafaframe('wine_data/test_wine_data.csv')
+
+
+# In[31]:
+
+
+train_df.head()
+
+
+# In[32]:
+
+
+validation_df.head()
+
+
+# In[33]:
+
+
+validation_df.info()
+
+
+# In[34]:
+
+
+#filling in the mean values
+
+for col, value in validation_df.items():
+ if col != 'type':
+    validation_df[col] = validation_df[col].fillna(validation_df[col].mean())
+validation_df.isnull().sum()
+
+
+# In[35]:
+
+
+trained_dict =  train_df.drop(columns=['type','quality'])
+val_dict =  validation_df.drop(columns=['type','quality'])
+
+
+# In[36]:
+
+
+X_train = trained_dict
+X_val = val_dict
+
+
+# In[37]:
+
+
+target = 'quality'
+y_train = train_df[target].values
+
+y_val = validation_df[target].values
+
+
+# In[39]:
+
+
+rfr.fit(X_train, y_train)
+y_pred_rfr = rfr.predict(X_val)
+
+with open('models/random_forest_reg.bin','wb') as f_out:
+    pickle.dump((rfr),f_out)
+
+rmse = mean_squared_error(y_val,y_pred_rfr,squared=False)
+accuracy = r2_score(y_val, y_pred_rfr)
+
+print('RandomForestRegressor')
+print(f'RMSE: {rmse}')
+print(f'Accuracy: {accuracy}')
+
+
+# In[40]:
+
+
+rfc.fit(X_train, y_train)
+y_pred_rfc = rfc.predict(X_val)
+
+with open('models/rfc.bin','wb') as f_out:
+    pickle.dump((rfc),f_out)
+
+rmse = mean_squared_error(y_val,y_pred_rfc,squared=False)
+accuracy = r2_score(y_val, y_pred_rfc)
+
+print('RandomForestClassifier')
+print(f'RMSE: {rmse}')
+print(f'Accuracy: {accuracy}')
+
+
+# In[41]:
+
+
+etc.fit(X_train, y_train)
+y_pred_etc = etc.predict(X_val)
+
+with open('models/etc.bin','wb') as f_out:
+    pickle.dump((etc),f_out)
+
+rmse = mean_squared_error(y_val,y_pred_etc,squared=False)
+accuracy = r2_score(y_val,y_pred_etc)
+
+print('ExtraTreesClassifier')
+print (f'RMSE: ',{rmse})
+print(f'Accuracy: ',{accuracy})
+
+
+# In[42]:
+
+
+knr = KNeighborsRegressor(n_neighbors=5)
+knr.fit(X_train, y_train)
+y_pred_knr = knr.predict(X_val)
+
+
+with open('models/kn_reg.bin','wb') as f_out:
+    pickle.dump((knr),f_out)
+
+rmse = mean_squared_error(y_val,y_pred_knr,squared=False)
+accuracy = r2_score(y_val, y_pred_knr)
+
+print('KNeighborsRegressor')
+print(f'RMSE: {rmse}')
+print(f'Accuracy: {accuracy}')
+
+
+# In[ ]:
+
+
+
+
